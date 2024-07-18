23eaebe1e941c62f7341ea2df79e8d6d2c4230c8
LEEVANCEE <1280403599@qq.com>
2024-07-19 04:18:49 +0800

    change navbar style

--- components/frontside/navbar.tsx
+++ components/frontside/navbar.tsx
@@ -3,37 +3,39 @@ import MainNav from './main-nav';
 import Container from '@/components/ui/container';
 import NavbarActions from './navbar-actions';
 import getCategories from '@/actions/get-categories';
-import {DropDown} from './DropDown';
-import {MobileSidebar} from './mobile-sidebar';
-import {ModeToggle} from '../modeToggle';
+import { DropDown } from './DropDown';
+import { MobileSidebar } from './mobile-sidebar';
+import { ModeToggle } from '../modeToggle';
 
 const STORE_ID = process.env.NEXT_PUBLIC_STORE_ID!;
 
 const Navbar = async () => {
-    const categories = await getCategories({storeId: STORE_ID});
-    return (
-        <header className="sticky inset-0 z-10  border-b bg-background/90 backdrop-blur-md">
-            <Container>
-                <nav className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-                    <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-                        <p className="font-bold text-xl">OGKSHOP</p>
-                    </Link>
-                    <div className="hidden lg:block">
-                        <MainNav data={categories}/>
-                    </div>
+  const categories = await getCategories({ storeId: STORE_ID });
+  return (
+    <header className="sticky inset-0 z-10 border-b bg-background/90 backdrop-blur-md">
+      <Container>
+        <nav className="px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
+          <div className="flex items-center">
+            <Link href="/" className="mr-4 flex lg:mr-0 gap-x-2">
+              <p className="font-bold text-xl">OGKSHOP</p>
+            </Link>
+            <div className="hidden lg:block">
+              <MainNav data={categories} />
+            </div>
+          </div>
 
-                    <div className="ml-auto flex items-center gap-x-4">
-                        <DropDown/>
-                        <NavbarActions/>
-                        <ModeToggle/>
-                    </div>
-                    <div className=" lg:hidden flex items-center justify-center h-full ml-3">
-                        <MobileSidebar categories={categories}/>
-                    </div>
-                </nav>
-            </Container>
-        </header>
-    );
+          <div className="flex items-center gap-x-4">
+            <DropDown />
+            <NavbarActions />
+            <ModeToggle />
+            <div className="lg:hidden items-center">
+              <MobileSidebar categories={categories} />
+            </div>
+          </div>
+        </nav>
+      </Container>
+    </header>
+  );
 };
 
 export default Navbar;
