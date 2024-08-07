4e37dff94fff53f0ec4dfda768a63e05cd731aec
LEEVANCEE <1280403599@qq.com>
2024-08-07 23:16:13 +0800

    fix delete function

--- app/api/[storeId]/products/[productId]/route.ts
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -28,8 +28,40 @@ export async function GET(req: Request, { params }: { params: { productId: strin
 }
 
 export async function DELETE(req: Request, { params }: { params: { productId: string; storeId: string } }) {
-  // DELETE 方法保持不变
-  // ...
+  try {
+    const session = await auth();
+    const userId = session?.user.id;
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 403 });
+    }
+
+    if (!params.productId) {
+      return new NextResponse('Product id is required', { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        adminId: userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 405 });
+    }
+
+    const product = await prismadb.product.delete({
+      where: {
+        id: params.productId,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log('[PRODUCT_DELETE]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
 }
 
 export async function PATCH(req: Request, { params }: { params: { productId: string; storeId: string } }) {
