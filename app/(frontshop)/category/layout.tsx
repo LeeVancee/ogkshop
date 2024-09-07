bf2a6b0ef4637c2576cac7fc96eba5045a7545cb
LEEVANCEE <1280403599@qq.com>
2024-09-08 04:20:22 +0800

    fix useSearchParams() should be wrapped in a suspense boundary at page

--- app/(frontshop)/category/layout.tsx
+++ app/(frontshop)/category/layout.tsx
@@ -1,5 +1,5 @@
 import Container from '@/components/ui/container';
-import React from 'react';
+import React, { Suspense } from 'react';
 import MobileFilters from './[categoryName]/components/mobile-filters';
 import Filter from './[categoryName]/components/filter';
 import getColors from '@/actions/get-colors';
@@ -7,17 +7,19 @@ import getSizes from '@/actions/get-sizes';
 
 const storeId = process.env.NEXT_PUBLIC_STORE_ID!;
 
-export default async function CategoryLayout({children}: { children: React.ReactNode }) {
+export default async function CategoryLayout({ children }: { children: React.ReactNode }) {
   const sizes = await getSizes(storeId);
   const colors = await getColors(storeId);
   return (
     <Container>
       <div className="p-6 sm:px-6 lg:px-8 pb-24">
         <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-          <MobileFilters sizes={sizes} colors={colors}/>
+          <MobileFilters sizes={sizes} colors={colors} />
           <div className="hidden lg:block">
-            <Filter valueKey="sizeName" name="Sizes" data={sizes}/>
-            <Filter valueKey="colorName" name="Colors" data={colors}/>
+            <Suspense>
+              <Filter valueKey="sizeName" name="Sizes" data={sizes} />
+              <Filter valueKey="colorName" name="Colors" data={colors} />
+            </Suspense>
           </div>
           {children}
         </div>
