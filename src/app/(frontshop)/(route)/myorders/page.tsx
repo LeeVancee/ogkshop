c5c4bbbfd6b4a5cbfc48b46edc86ca378c06b3b2
LEEVANCEE <1280403599@qq.com>
2024-11-06 04:48:38 +0800

    migrate to src folder

--- src/app/(frontshop)/(route)/myorders/page.tsx
+++ src/app/(frontshop)/(route)/myorders/page.tsx
@@ -1,9 +1,4 @@
-import { auth } from '@/auth';
 import Container from '@/components/ui/container';
-import prismadb from '@/lib/prismadb';
-import { formatter } from '@/lib/utils';
-import { format } from 'date-fns';
-import { OrderColumn } from '@/types';
 import OrderList from './components/OrderList';
 import { Suspense } from 'react';
 
